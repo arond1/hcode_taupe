f72f7f0584970aede37bc66a839f9a87882bdb8f
arond1 <aron4733@gmail.com>
2020-02-07 11:43:25 +0100

    f

--- parser.py
+++ parser.py
@@ -1,6 +1,24 @@
 import solveur
+import os
+
 def parser():
-    slice = 17
-    types = 4
-    values = [2, 5, 6, 8]
+    f = open('./hcode_taupe/a_example.in', "r")
+    lines1 = f.readlines()
+    f.close()
+    print(lines1)
+    line_one = lines1[0].split(' ')
+    line_two = lines1[1].split(' ')
+    print(line_one)
+    print(line_two)
+    line_one[1] = line_one[1].split('\n')[0]
+    print(line_one)
+    line_two[len(line_two)-1] = line_two[len(line_two)-1].split('\n')[0]
+    print(line_two)
+    slice = int(line_one[0])
+    types = int(line_one[1])
+    values = []
+    for i in line_two:
+        values.append(int(i))
     solveur.solveur(slice, types, values)
+
+parser()
